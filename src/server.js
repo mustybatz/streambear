174d155496723b5e5165a5c858fa2a62710a7b25
Barush Mendez <barush_mendez-3.0@hotmail.com>
2020-08-25 11:07:03 -0500

    Commit inicial, aplicación funcional.

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const initSignaling = require('./signaling');
+const http = require("http");
+
+const app = express();
+const port = 4000;
+const server = http.createServer(app);
+initSignaling(server);
+
+app.use(express.static(__dirname + "/public"));
+
+server.listen(port, () => console.log(`Server is running on port ${port}`));
---
+++ src/signaling.js
@@ -0,0 +1,36 @@
+const initSignaling = (server) => {
+    const io = require('socket.io')(server);
+    let broadcaster;
+    io.sockets.on("error", e => console.log(e));
+
+    io.sockets.on("connection", socket => {
+
+        socket.on("broadcaster", () => {
+            broadcaster = socket.id;
+            socket.broadcast.emit("broadcaster");
+        });
+
+        socket.on("watcher", () => {
+            socket.to(broadcaster).emit("watcher", socket.id);
+        });
+
+        socket.on("offer", (id, message) => {
+            socket.to(id).emit("offer", socket.id, message);
+        });
+
+        socket.on("answer", (id, message) => {
+            socket.to(id).emit("answer", socket.id, message);
+        });
+
+        socket.on("candidate", (id, message) => {
+            socket.to(id).emit("candidate", socket.id, message);
+        });
+
+        socket.on("disconnect", () => {
+            socket.to(broadcaster).emit("disconnectPeer", socket.id);
+        });
+
+    });
+}
+
+module.exports = initSignaling;
